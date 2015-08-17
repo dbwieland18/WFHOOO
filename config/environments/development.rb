d887f2088c056d1f45c86449f6ec21b16320646b
Devin Wieland <devinwieland@gmail.com>
2015-08-17 10:21:19 -0400

    removes creds

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            'devinwieland',
-    password:             'packers1986',
+    user_name:            '',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
